276d775ea7cf09ab6b455983d00a20399f2f5345
Faruq Hossen <faruqhossen30@gmail.com>
2021-10-03 11:28:46 +0600

    topbar marchant login add

--- resources/views/frontend/layouts/topbar.blade.php
+++ resources/views/frontend/layouts/topbar.blade.php
@@ -1,19 +1,19 @@
 <div class="header-top">
     <div class="container">
         <div class="header-left">
-            <p class="welcome-msg">Welcome to Riode store message or remove it!</p>
+            <p class="welcome-msg">HeshelGhor | Stores Of Needs</p>
         </div>
         <div class="header-right">
-            <div class="dropdown">
+            {{-- <div class="dropdown">
                 <a href="#currency">USD</a>
                 <ul class="dropdown-box">
                     <li><a href="#USD">USD</a></li>
                     <li><a href="#EUR">EUR</a></li>
                 </ul>
-            </div>
+            </div> --}}
             <!-- End DropDown Menu -->
             <div class="dropdown ml-5">
-                <a href="#language">ENG</a>
+                <a href="#language">BAN</a>
                 <ul class="dropdown-box">
                     <li>
                         <a href="#USD">ENG</a>
@@ -25,6 +25,7 @@
             </div>
             <!-- End DropDown Menu -->
             <span class="divider"></span>
+            <a href="{{route('marchant.login')}}" class="contact d-lg-show"><i class="d-icon-percent"></i>Merchant</a>
             <a href="contact-us.html" class="contact d-lg-show"><i class="d-icon-map"></i>Contact</a>
             <a href="#" class="help d-lg-show"><i class="d-icon-info"></i> Need Help</a>
             <a class="login-link" href="ajax/login.html" data-toggle="login-modal"><i
