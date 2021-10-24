1f76a1e6b6184249b85a045268a8b2235ae1ec9b
Sahadat Hossain <sahadatjh@gmail.com>
2021-10-24 23:54:00 +0600

    devlivery address add api done

--- app/Http/Controllers/API/User/UserOrderAPIController.php
+++ app/Http/Controllers/API/User/UserOrderAPIController.php
@@ -65,13 +65,51 @@ class UserOrderAPIController extends Controller
                 'payment_method_id',
                 'status'
             );
-            $oder_item_id = DB::table('order_items')->insertGetId($order_item_data);
+            // $oder_item_id = DB::table('order_items')->insertGetId($order_item_data);
+            // $inserted_item = DB::table('order_items')->find($oder_item_id);
 
+            $oder_item_id = OrderItem::insertGetId($order_item_data);
+            $inserted_item = OrderItem::find($oder_item_id);
 
             return response()->json([
                 'success' => true,
                 'message' => 'Order Created successfully!',
-                'data'    => $oder_item_id
+                'data'    => $inserted_item
+            ], 201);
+        } catch (Exception $e) {
+            $exception_code = $e->getCode();
+            $response_code = $exception_code < 200 || $exception_code > 500 ? 500 : $exception_code;
+            return response()->json($this->catch_exception($e), $response_code);
+        }
+    }
+
+    public function deliveryAddress(Request $request)
+    {
+        try {
+            $order_item_data = $request->only(
+                'name',
+                'user_id',
+                'order_id',
+                'email',
+                'company',
+                'address',
+                'message',
+                'zip_code',
+                'mobile',
+                'division_id',
+                'district_id',
+                'upazila_id',
+            );
+            // $oder_item_id = DB::table('order_items')->insertGetId($order_item_data);
+            // $inserted_item = DB::table('order_items')->find($oder_item_id);
+
+            $address_id = DeliveryAddress::insertGetId($order_item_data);
+            $inserted_address = DeliveryAddress::find($address_id);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Delivery Address Created successfully!',
+                'data'    => $inserted_address
             ], 201);
         } catch (Exception $e) {
             $exception_code = $e->getCode();
