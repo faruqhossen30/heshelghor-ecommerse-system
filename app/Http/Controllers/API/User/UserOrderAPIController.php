d3c601f4a3c0a546436e01b5d636dc29523bee27
Faruq Hossen <faruqhossen30@gmail.com>
2021-10-30 12:16:47 +0600

    product order item deliveryaddress api update again

--- app/Http/Controllers/API/User/UserOrderAPIController.php
+++ app/Http/Controllers/API/User/UserOrderAPIController.php
@@ -9,22 +9,34 @@ use App\Models\Merchant\OrderItem;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Models\Admin\Order\DeliveryAddress;
+use Illuminate\Support\Facades\Validator;
 
 class UserOrderAPIController extends Controller
 {
     public function createOrder(Request $request)
     {
-        // $request->validate([
-        //     'name'              => 'required',
-        //     'address'           => 'required',
-        //     'mobile'            => 'required',
-        //     'email'             => 'required',
-        //     'division_id'       => 'required',
-        //     'district_id'       => 'required',
-        //     'upazila_id'        => 'required',
-        //     'delivery_system'   => 'required',
-        //     'payment_method_id' => 'required'
-        // ]);
+        $data = $request->all();
+
+        $rules = [
+            'user_id'            => 'required',
+            'order_no'           => 'required',
+            'total_prodcut'      => 'required',
+            'total_item'         => 'required',
+            'delivery_cost'      => 'required',
+            'product_price'      => 'required',
+            'total_price'        => 'required',
+            'delivery_system_id' => 'required',
+            'payment_method_id'  => 'required'
+        ];
+        $errorMessage = [
+            'user_id.required' => 'Please input your Uuser id '
+        ];
+
+        $validator = Validator::make($data, $rules, $errorMessage);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
 
         $order = Order::create([
             'user_id'            => $request->user_id,
@@ -41,80 +53,111 @@ class UserOrderAPIController extends Controller
         return response()->json([
             'success' => true,
             'code'    => 201,
-            'message' => 'Data create successfully',
+            'message' => 'Order create successfully',
             'data'    => $order
         ]);
     }
 
     public function createOrderitem(Request $request)
     {
-        try {
-            $order_item_data = $request->only(
-                'user_id',
-                'merchant_id',
-                'order_id',
-                'product_id',
-                'regular_price',
-                'discount',
-                'price',
-                'quantity',
-                'color',
-                'size',
-                'order_No',
-                'delivery_system_id',
-                'payment_method_id',
-                'status'
-            );
-            // $oder_item_id = DB::table('order_items')->insertGetId($order_item_data);
-            // $inserted_item = DB::table('order_items')->find($oder_item_id);
+        $data = $request->all();
 
-            $oder_item_id = OrderItem::insertGetId($order_item_data);
-            $inserted_item = OrderItem::find($oder_item_id);
+        $rules = [
+            'user_id'            => 'required',
+            'merchant_id'        => 'required',
+            'order_id'           => 'required',
+            'product_id'         => 'required',
+            'regular_price'      => 'required',
+            'discount'           => 'required',
+            'price'              => 'required',
+            'quantity'           => 'required',
+            'color'              => 'required',
+            'size'               => 'required',
+            'order_No'           => 'required',
+            'payment_method_id'  => 'required',
+            'delivery_system_id' => 'required',
+        ];
+        $errorMessage = [
+            'user_id.required' => 'Please input your User id '
+        ];
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Order Created successfully!',
-                'data'    => $inserted_item
-            ], 201);
-        } catch (Exception $e) {
-            $exception_code = $e->getCode();
-            $response_code = $exception_code < 200 || $exception_code > 500 ? 500 : $exception_code;
-            return response()->json($this->catch_exception($e), $response_code);
+        $validator = Validator::make($data, $rules, $errorMessage);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
         }
+
+
+        $orderItem = OrderItem::create([
+            'user_id'           => $request->user_id,
+            'merchant_id'       => $request->merchant_id,
+            'order_id'          => $request->order_id,
+            'product_id'        => $request->product_id,
+            'regular_price'     => $request->regular_price,
+            'discount'          => $request->discount,
+            'price'             => $request->price,
+            'quantity'          => $request->quantity,
+            'color'             => $request->color,
+            'size'              => $request->size,
+            'order_No'          => $request->order_No,
+            'payment_method_id' => $request->payment_method_id,
+            'delivery_system_id' => $request->delivery_system_id,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'code'    => 201,
+            'message' => 'Order Item create successfully',
+            'data'    => $orderItem
+        ]);
     }
 
     public function deliveryAddress(Request $request)
     {
-        try {
-            $order_item_data = $request->only(
-                'name',
-                'user_id',
-                'order_id',
-                'email',
-                'company',
-                'address',
-                'message',
-                'zip_code',
-                'mobile',
-                'division_id',
-                'district_id',
-                'upazila_id',
-            );
-            // $oder_item_id = DB::table('order_items')->insertGetId($order_item_data);
-            // $inserted_item = DB::table('order_items')->find($oder_item_id);
+        $data = $request->all();
+        $rules = [
+            'name'        => 'required',
+            'user_id'     => 'required',
+            'order_id'    => 'required',
+            'email'       => 'required|email',
+            'address'     => 'required',
+            'mobile'      => 'required',
+            'division_id' => 'required',
+            'district_id' => 'required',
+            'upazila_id'  => 'required'
+        ];
 
-            $address_id = DeliveryAddress::insertGetId($order_item_data);
-            $inserted_address = DeliveryAddress::find($address_id);
+        $errorMessage = [
+            'user_id.required' => 'Please input your User id '
+        ];
 
-            return response()->json([
-                'success' => true,
-                'message' => 'Delivery Address Created successfully!',
-                'data'    => $inserted_address
-            ], 201);
-        } catch (Exception $e) {
-            $exception_code = $e->getCode();
-            $response_code = $exception_code < 200 || $exception_code > 500 ? 500 : $exception_code;
-            return response()->json($this->catch_exception($e), $response_code);
+        $validator = Validator::make($data, $rules, $errorMessage);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
         }
+
+        $deliveryAddress = DeliveryAddress::create([
+            'name'              => $request->name,
+            'user_id'           => $request->user_id,
+            'order_id'          => $request->order_id,
+            'email'             => $request->email,
+            'company'           => $request->company,
+            'address'           => $request->address,
+            'message'           => $request->message,
+            'zip_code'          => $request->zip_code,
+            'mobile'            => $request->mobile,
+            'division_id'       => $request->division_id,
+            'district_id'       => $request->district_id,
+            'payment_method_id' => $request->payment_method_id,
+            'upazila_id'        => $request->upazila_id
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'code'    => 201,
+            'message' => 'Delivery address create successfully',
+            'data'    => $deliveryAddress
+        ]);
+
+
     }
 }
