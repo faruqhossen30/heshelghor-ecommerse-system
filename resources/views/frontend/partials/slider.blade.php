9ae90a24d9ff6a1cef491280dbb22157332d8da7
Faruq Hossen <faruqhossen30@gmail.com>
2022-02-06 14:47:28 +0600

    slider image change

--- resources/views/frontend/partials/slider.blade.php
+++ resources/views/frontend/partials/slider.blade.php
@@ -9,10 +9,10 @@
     }">
         <div class="banner banner-fixed intro-slide1">
             <figure>
-                <img src="{{asset('uploads/slider/banners1.png')}}" alt="intro-banner" width="880"
-                    height="474" style="background-color: #ffc74e;" />
+                <img src="{{asset('frontend/images/slider/banner1.png')}}" alt="intro-banner" width="800"
+                    height="400" style="background-color: #ffc74e; min-height:0px" />
             </figure>
-            <div class="banner-content y-50 pb-3">
+            {{-- <div class="banner-content y-50 pb-3">
                 <div class="slide-animate" data-animation-options="{
                     'name': 'fadeInUpShorter',
                     'duration': '1s'
@@ -26,46 +26,15 @@
                     </p>
                     <a href="demo3-shop.html" class="btn btn-white btn-rounded text-white" style="background: #28475c">Shop Now</a>
                 </div>
-            </div>
+            </div> --}}
 
         </div>
-        <div class="banner banner-fixed intro-slide2">
+        <div class="banner banner-fixed intro-slide1">
             <figure>
-                <img src="{{asset('uploads/slider/banners2.png')}}" alt="intro-banner" width="880"
-                    height="474" style="background-color: #fbfcfc;" />
+                <img src="{{asset('frontend/images/slider/banner2.png')}}" alt="intro-banner" width="800"
+                    height="400" style="background-color: #ffc74e; min-height:0px" />
             </figure>
-            <div class="banner-content y-50 text-right">
-                <h4 class="banner-subtitle text-body font-weight-semi-bold mb-3 slide-animate"
-                    data-animation-options="{
-                    'name': 'fadeInDownShorter',
-                    'duration': '1s'
-                }">Up To <strong class="text-primary">25% Off</strong></h4>
-                <h2 class="banner-title text-uppercase ls-l slide-animate"
-                    data-animation-options="{
-                    'name': 'fadeInDownShorter',
-                    'delay': '.3s',
-                    'duration': '1s'
-                }">For Women’s</h2>
-                <p class="font-weight-semi-bold ls-m text-body mb-6 slide-animate"
-                    data-animation-options="{
-                    'name': 'fadeInDownShorter',
-                    'delay': '.4s',
-                    'duration': '1s'
-                }">Start at $12.00</p>
-                <a href="demo3-shop.html" class="btn btn-dark btn-rounded slide-animate"
-                    data-animation-options="{
-                    'name': 'fadeInDownShorter',
-                    'delay': '.5s',
-                    'duration': '1s'
-                }">Shop Now</a>
-            </div>
-        </div>
-        <div class="banner banner-fixed intro-slide3">
-            <figure>
-                <img src="{{asset('uploads/slider/banners1.png')}}" alt="intro-banner" width="880"
-                    height="474" style="background-color: #d3d4d5;" />
-            </figure>
-            <div class="banner-content y-50 pb-3">
+            {{-- <div class="banner-content y-50 pb-3">
                 <div class="slide-animate" data-animation-options="{
                     'name': 'fadeInUpShorter',
                     'duration': '1s'
@@ -77,9 +46,54 @@
                             class="text-primary">$29.00</strong></h4>
                     <p class="text-dark lh-1 ls-m mb-4">* Get Plus Discount Buying Package
                     </p>
-                    <a href="demo3-shop.html" class="btn btn-white btn-rounded">Shop Now</a>
+                    <a href="demo3-shop.html" class="btn btn-white btn-rounded text-white" style="background: #28475c">Shop Now</a>
                 </div>
-            </div>
+            </div> --}}
+
+        </div>
+        <div class="banner banner-fixed intro-slide1">
+            <figure>
+                <img src="{{asset('frontend/images/slider/banner3.png')}}" alt="intro-banner" width="800"
+                    height="400" style="background-color: #ffc74e; min-height:0px" />
+            </figure>
+            {{-- <div class="banner-content y-50 pb-3">
+                <div class="slide-animate" data-animation-options="{
+                    'name': 'fadeInUpShorter',
+                    'duration': '1s'
+                }">
+                    <h4 class="banner-subtitle font-weight-normal ls-m mb-1">Deals and
+                        Promotions</h4>
+                    <h2 class="banner-title text-uppercase mb-3 ls-l">Season Clothing</h2>
+                    <h4 class="banner-price-info text-uppercase ls-l">Start at <strong
+                            class="text-primary">$29.00</strong></h4>
+                    <p class="text-dark lh-1 ls-m mb-4">* Get Plus Discount Buying Package
+                    </p>
+                    <a href="demo3-shop.html" class="btn btn-white btn-rounded text-white" style="background: #28475c">Shop Now</a>
+                </div>
+            </div> --}}
+
+        </div>
+        <div class="banner banner-fixed intro-slide1">
+            <figure>
+                <img src="{{asset('frontend/images/slider/banner4.png')}}" alt="intro-banner" width="800"
+                    height="400" style="background-color: #ffc74e; min-height:0px" />
+            </figure>
+            {{-- <div class="banner-content y-50 pb-3">
+                <div class="slide-animate" data-animation-options="{
+                    'name': 'fadeInUpShorter',
+                    'duration': '1s'
+                }">
+                    <h4 class="banner-subtitle font-weight-normal ls-m mb-1">Deals and
+                        Promotions</h4>
+                    <h2 class="banner-title text-uppercase mb-3 ls-l">Season Clothing</h2>
+                    <h4 class="banner-price-info text-uppercase ls-l">Start at <strong
+                            class="text-primary">$29.00</strong></h4>
+                    <p class="text-dark lh-1 ls-m mb-4">* Get Plus Discount Buying Package
+                    </p>
+                    <a href="demo3-shop.html" class="btn btn-white btn-rounded text-white" style="background: #28475c">Shop Now</a>
+                </div>
+            </div> --}}
+
         </div>
     </div>
 </section>
