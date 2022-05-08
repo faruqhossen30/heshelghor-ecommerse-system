50eee6b191c1e9f95263749fc3a595c4045c0ede
Faruq Hossen <faruqhossen30@gmail.com>
2022-05-08 11:29:21 +0600

    slider banner updated

--- resources/views/frontend/partials/slider.blade.php
+++ resources/views/frontend/partials/slider.blade.php
@@ -46,7 +46,7 @@
 
         </div>
         <div class="banner banner-fixed intro-slide1">
-            <a href="{{ route('promotion') }}">
+            <a href="{{ route('marchant.login') }}">
                 <figure>
                     <img src="{{ asset('frontend/images/slider/new.jpeg') }}" alt="intro-banner" width="800"
                         height="400" style="background-color: #ffc74e; min-height:0px" />
