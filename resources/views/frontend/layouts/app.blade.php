2e454c4e7a2acd750873192dc1ef632dfdc1ffb9
Faruq Hossen <faruqhossen30@gmail.com>
2022-06-29 17:40:12 +0600

    single page OG tag update

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" />
     <link rel="stylesheet" href="{{ asset('frontend') }}/css/jquery.exzoom.css" />
     <script src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
+    @yield('OG')
 
     @stack('style')
 
