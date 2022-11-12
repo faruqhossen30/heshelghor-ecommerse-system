431579744d2dc841fa7c3b282fb7bf03f1c64477
aminulcoder <aminulislammd97@gmail.com>
2022-11-12 16:41:46 +0600

    cu controller added

---
+++ app/Http/Controllers/Ajax/FrontEnd/CuajaxController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Ajax\FrontEnd;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CuajaxController extends Controller
+{
+    //
+}
